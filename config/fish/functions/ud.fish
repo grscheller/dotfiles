2363d475b00ce2132d95a37ee4dced3112ee0781
grscheller <geoffrey@scheller.com>
2021-07-01 17:19:28 -0600

    Commit changes config/fish/functions/ud.fish - untested

--- config/fish/functions/ud.fish
+++ config/fish/functions/ud.fish
@@ -1,11 +1,25 @@
 function ud --description 'Jump up multiple directories, default is 1'
-    set -l upDir ..
-    set -l nDirs $argv[1]
+    set upDir ..
+    set findDir ''
+    set nDirs $argv[1]
     if string match -qr '[1-9]\d*' $nDirs
         while [ $nDirs -gt 1 ]
             set upDir ../$upDir
             set nDirs (math $nDirs - 1)
         end
+    else
+        set findDir (string trim $nDirs)
+        if [ -n $findDir ]
+            set cnt 1
+            set maxUp (math (string split / (pwd) | count) - 1)
+            while [ $cnt -le $maxUp ]
+                test -d $upDir/$findDir
+                and break
+                set upDir ../$upDir
+                set cnt (math $cnt + 1)
+            end
+        end
     end
-    cd $upDir
+    test -d
+    and cd $upDir/$findDir
 end
