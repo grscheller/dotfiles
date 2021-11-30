0f8786db075846b6108c08d28cb5506e94806289
grscheller <geoffrey@scheller.com>
2021-11-29 21:22:59 -0700

    Updated ~/.config/fish/config.fish
    Switched last two lines, want user's version
    to take precedence.
    
        # Ruby tool chain
        #   Mostly for locally installed ruby gems,
        #     to install these:
        #       Markdown linter: $ gem install mdl
        #       Neovim syntax:   $ gem install neovim
        set -p PATH /usr/local/opt/ruby/bin
        set -p PATH ~/.local/share/gem/ruby/*/bin

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -36,8 +36,8 @@ and begin
     #     to install these:
     #       Markdown linter: $ gem install mdl
     #       Neovim syntax:   $ gem install neovim
-    set -p PATH ~/.local/share/gem/ruby/*/bin
     set -p PATH /usr/local/opt/ruby/bin
+    set -p PATH ~/.local/share/gem/ruby/*/bin
 
     # Rust toolchain
     set -p PATH ~/.cargo/bin
