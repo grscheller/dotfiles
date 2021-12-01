e24ff2aebd813475219cb2f43a050ca13ac3778f
grscheller <geoffrey@scheller.com>
2021-12-01 02:13:50 -0700

    Gave config/fish/functions/tm.fish more options
     - tm -x: try running Alacritty under XWayland
     - tm -w: run Alacritty under Wayland if possible

--- config/fish/functions/tm.fish
+++ config/fish/functions/tm.fish
@@ -4,19 +4,21 @@
 # which is semi-functional, does not display title, and is bright
 # and annoying.  This can be avoided by running Alacritty
 # under XWayland.  That said, we still want the fish shell
-# running in Alacritty to be aware of the Wayland environment
+# running in Alacritty to be aware of a Wayland environment
 # and have any launched applications run under Wayland, not XWayland.
 function tm --description 'Launch fish shell running in Alacritty terminal'
 
     set -g _tm_do_it yes
-    set WD ()
-    set WD_ORIG $WAYLAND_DISPLAY
+    set wFlag ()
+    set xFlag ()
+    set WD $WAYLAND_DISPLAY
 
     # Define private functions
     function __tm_usage
-        printf '\nUsage: tm [-w] [-h]' >&2
+        printf '\nUsage: tm [-w|-x] [-h]' >&2
         printf '\n  where' >&2
-        printf '\n    -w: run alacrity under wayland' >&2
+        printf '\n    -w: try running alacrity under Wayland' >&2
+        printf '\n    -x: try running alacrity under XWayland or Xorg' >&2
         printf '\n    -h: show help for tm' >&2
     end
 
@@ -39,22 +41,38 @@ function tm --description 'Launch fish shell running in Alacritty terminal'
     set index 1
     set cnt (count $argv)
     while [ $index -le $cnt ]
-        switch $argv[$index]
-            case -h --help -help
-                __tm_punt
-                break
-            case -w --wayland
-                set WD $WD_ORIG
+      switch $argv[$index]
+        case -h --help -help
+            __tm_punt
+            break
+        case -w --wayland
+            if [ -z $xFlag[1] ]
+                set wFlag yes
                 set index (math "$index+1")
-            case '*'
-                __tm_punt "Unexpected option or argument \"$argv[$index]\" given"
+            else
+                __tm_punt 'Options -w and -x both cannot be set'
                 break
-        end
+            end
+        case -x --xwayland --xorg
+            if [ -z $wFlag[1] ]
+                set xFlag yes
+                set WD ()
+                set index (math "$index+1")
+            else
+                __tm_punt 'Options -w and -x both cannot be set'
+                break
+            end
+        case '*'
+            __tm_punt "Unexpected option or argument \"$argv[$index]\" given"
+            break
+      end
     end
 
     # Launch alacritty, if not punting
     if [ $_tm_do_it[1] = yes ]
-        WAYLAND_DISPLAY=$WD alacritty -e fish -C "set -x WAYLAND_DISPLAY $WD_ORIG" &
+        WAYLAND_DISPLAY=$WD \
+        alacritty -e fish \
+          -C "set -x WAYLAND_DISPLAY $WAYLAND_DISPLAY" &
         disown
     end
 
