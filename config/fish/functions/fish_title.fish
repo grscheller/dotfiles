2ed0dda2cf5700e621814b32fede36a13efdabb1
grscheller <geoffrey@scheller.com>
2022-01-12 10:01:18 -0700

    Enabled fish to terminal title swap like bash
     - updated ~/.config/fish/functions/fish_title.fish
     - updated SpaceCAMP31 terminal title sc31 -> SC31

--- config/fish/functions/fish_title.fish
+++ config/fish/functions/fish_title.fish
@@ -1,3 +1,10 @@
 function fish_title --description 'Set terminal title'
-    printf '%s@%s' (id -un) (hostnamectl hostname)
+    set MyHostName (hostnamectl hostname)
+    switch $MyHostName[1]
+        case 'rvsllschellerg2'
+            set MyHostName voltron
+        case 'SpaceCAMP31'
+            set MyHostName SC31
+    end
+    printf '%s@%s' (id -un) $MyHostName
 end
