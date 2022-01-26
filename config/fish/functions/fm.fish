984beb838f694bb0970c3ec573d3cee0cf7eb252
grscheller <geoffrey@scheller.com>
2022-01-26 16:41:10 -0700

    Updated config/fish/functions/fm.fish
     - nemo needs to be launched in background in sway
     - gagged error/warning messages in shell
     - removed some unnecessay bashisms

--- config/fish/functions/fm.fish
+++ config/fish/functions/fm.fish
@@ -1,5 +1,9 @@
 function fm --description 'Launch desktop\'s file manager app'
-    set -l DiR "$argv[1]"
-    test -n $DiR[1] || set DiR "$PWD"
-    xdg-open "$DiR[1]"
+    set -l DiR $argv[1]
+    test -z $DiR; and set DiR $PWD
+    if test -d $DiR
+        xdg-open $DiR 2>/dev/null &
+    else
+        printf 'Error: "%s" is not a directory\n' $DiR
+    end
 end
