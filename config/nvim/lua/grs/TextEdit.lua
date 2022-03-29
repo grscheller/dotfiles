56c2c25a0290ec864ac3e2b8764a74d8ad6d80c6
grscheller <geoffrey@scheller.com>
2022-03-29 07:50:24 -0600

    Updated nvim grs.TextEdit config
     - Added cmd descriptions for
       - tpope/vim-commentary
       - justtinmk/vim-sneak
       - tpope/vim-surround
       - tpope/vim-repeat
     - maybe I should move these to neovim-notes repo?

--- config/nvim/lua/grs/TextEdit.lua
+++ config/nvim/lua/grs/TextEdit.lua
@@ -1,18 +1,22 @@
---[[ Contains configurations for basic text editting plugins
+--[[ Contains configurations for basic text editing using plugins
 
        Module: grs
        File: ~/.config/nvim/lua/grs/TextEdit.lua
 
      Text editing plugins installed via Packer:
        tpope/vim-commentary 
-       tpope/vim-surround
-       tpope/vim-repeat
        justtinmk/vim-sneak
-  ]]
+       tpope/vim-surround
+       tpope/vim-repeat     ]]
 
 local wk = require('grs.WhichKey')
 
---[[ Vim-Sneak
+--[[ Vim Commentary - comment out sections of code quickly
+
+     gc<motion>  -- in normal mode
+     gc          -- in visualmode  ]]
+
+--[[ Vim Sneak - 2 chaacter, multiline version of f & t
 
      s<Char><Char>  -- like a 2 char f (but multiline)
      S<Char><Char>  -- like a 2 char F (but multiline)
@@ -28,3 +32,41 @@ if wk then
 else
   print('Vim-Sneak keybindings setup failed')
 end
+
+--[[ Vim Surround - manipulate matching surrounding symbols
+
+     I. In normal mode,
+
+       ds"  -- "foo bar" -> foo bar
+       ds(  -- foo({ foo = bar, num = 42 }) -> foo{ foo = bar, num = 42 }
+       dst  -- delete surrounding HTML tags
+       cs'[ -- 'hello 42' -> [ hello 42 ]
+       cs'] -- 'hello 42' -> [hello 42]
+
+     In an html filetype:
+       cst<p> -- <title>My homepage</title> -> <p>My homepage</p>
+
+     Cursor is on the 'r'
+       ysiw) -- Hello world -> Hello (world)
+
+     Can act on ebtire line after initial whitespace - 
+       yss{ -- printf("Hello World\n") -> { printf("Hello World\n") }
+       ySS{ -- printf("Hello World\n") -> {
+                                            printf("Hello World\n")
+                                          }
+     II, In insert mode, where | means cursor,
+
+       foo = bar<C-S>( -> foo = bar( | )
+       foo<C-G>s( -> foo( | )
+       do <C-G>S{ -> do {
+                       |
+                     }
+
+     III, In visual mode, 
+
+       S"  -- wrap visual selection in "
+       gS{ -- wrap selection across multiple lines in {
+  ]]
+
+--[[ Vim Repeat - allows opted in plugins to repeat actions via . ]]
+
