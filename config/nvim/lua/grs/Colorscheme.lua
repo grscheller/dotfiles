f3a0fe12d78c1e55eb6057c2e17b4c9a1d636d8b
grscheller <geoffrey@scheller.com>
2022-06-19 13:44:54 -0600

    Tweaked colorschemes for Neovim & alacritty
     - alacritty & nvim terminal running on alacritty nearly identical
     - went with totally "#000000" black background

--- config/nvim/lua/grs/Colorscheme.lua
+++ config/nvim/lua/grs/Colorscheme.lua
@@ -17,12 +17,12 @@ end
 if pcall(require, 'tokyonight') then
   vim.g.tokyonight_style = 'night'
   vim.g.tokyonight_colors = {
-    bg = '#0d0e13',
+    bg = '#000000',
     fg = '#c0caf5',
-    comment = '#818ecd'
+    comment = '#818ecd',
+    cyan = '#0cb4c0'
   }
   vim.g.tokyonight_italic_functions = 1
-  vim.g.tokyonight_sidebars = {'qf', 'vista_kind', 'terminal', 'packer'}
   vim.cmd [[ colorscheme tokyonight ]]
 else
   vim.cmd [[ colorscheme darkblue ]]
