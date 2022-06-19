76838cad040112e71d6646cc99489157b3a1be9b
grscheller <geoffrey@scheller.com>
2022-06-19 16:18:54 -0600

    Added 16 base conversion fish functions
     - d2h, h2d, b2h, o2d, h2h, ...
     - /usr/bin/bc based
     - compactly put into ~/.config/fish/config.fish
       - did not want to clutter ~/.config/fish/functions/
         with 16 scattered new names

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -135,6 +135,25 @@ abbr -a -g gm git mv
 abbr -a -g gp git pull
 abbr -a -g gh git push
 abbr -a -g gs git status  # gs steps on ghostscript
+
 # Shell environment cmds
 abbr -a -g -- re REDO_ENV=yes fish -l -C cd
 abbr -a -g ue UPDATE_ENV=yes fish
+
+# Convert between various bases (use capital A-F for hex-digits)
+function h2h; printf 'ibase=16\nobase=10\n%s\n'   "$argv" | /usr/bin/bc; end
+function h2d; printf 'ibase=16\nobase=A\n%s\n'    "$argv" | /usr/bin/bc; end
+function h2o; printf 'ibase=16\nobase=8\n%s\n'    "$argv" | /usr/bin/bc; end
+function h2b; printf 'ibase=16\nobase=2\n%s\n'    "$argv" | /usr/bin/bc; end
+function d2h; printf 'ibase=10\nobase=16\n%s\n'   "$argv" | /usr/bin/bc; end
+function d2d; printf 'ibase=10\nobase=10\n%s\n'   "$argv" | /usr/bin/bc; end
+function d2o; printf 'ibase=10\nobase=8\n%s\n'    "$argv" | /usr/bin/bc; end
+function d2b; printf 'ibase=10\nobase=2\n%s\n'    "$argv" | /usr/bin/bc; end
+function o2h; printf 'ibase=8\nobase=20\n%s\n'    "$argv" | /usr/bin/bc; end
+function o2d; printf 'ibase=8\nobase=12\n%s\n'    "$argv" | /usr/bin/bc; end
+function o2o; printf 'ibase=8\nobase=10\n%s\n'    "$argv" | /usr/bin/bc; end
+function o2b; printf 'ibase=8\nobase=2\n%s\n'     "$argv" | /usr/bin/bc; end
+function b2h; printf 'ibase=2\nobase=10000\n%s\n' "$argv" | /usr/bin/bc; end
+function b2d; printf 'ibase=2\nobase=1010\n%s\n'  "$argv" | /usr/bin/bc; end
+function b2o; printf 'ibase=2\nobase=1000\n%s\n'  "$argv" | /usr/bin/bc; end
+function b2b; printf 'ibase=2\nobase=10\n%s\n'    "$argv" | /usr/bin/bc; end
