054b9adf00e62e777e18e30eb9e5d94c0cac843c
grscheller <geoffrey@scheller.com>
2022-07-12 17:28:45 -0600

    Added git info to fish prompt
     - ~/.config/fish/functions/fish_prompt.fish

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,14 @@
 function fish_prompt --description 'Customize prompt'
+    set -g __fish_git_prompt_showupstream auto verbose
+    set -g __fish_git_prompt_showstashstate
+    set -g __fish_git_prompt_showdirtystate
+    set -g __fish_git_prompt_describe_style branch
+    set -g __fish_git_prompt_use_informative_chars
+    set -g __fish_git_prompt_showcolorhints
+    set -g __fish_git_prompt_color cyan
     set_color cyan
-    printf '\n[%s: %s]\n$ ' (hostnamectl hostname) (string replace -r "^$HOME" '~' (pwd))
+    printf '\n[%s: %s]%s' (hostnamectl hostname) (string replace -r "^$HOME" '~' (pwd)) (fish_git_prompt) 
+    set_color cyan
+    printf '\n$ '
     set_color normal
 end
