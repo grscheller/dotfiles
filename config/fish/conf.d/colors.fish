c408afd1af2557586d76185c14f56aa2b682a396
grscheller <geoffrey@scheller.com>
2022-07-28 07:35:14 -0600

    Beginning to tweak my fish TokyoNight colorscheme
     - trying to more or less match what nvim treesitter
       does with the nvim TokyoNight based scheme I use.

--- config/fish/conf.d/colors.fish
+++ config/fish/conf.d/colors.fish
@@ -5,15 +5,16 @@ if not set -q tokyo_night_colors_set
     # TokyoNight based colors consistent with my alacritty configuration
     set -l foreground c0caf5  #c0caf5  bright white
     set -l selection 33467c   #33467c
-    set -l comment 565f89     #565f89
+    set -l comment 818ecd     #818ecd
     set -l red f7768e         #f7768e  red
     set -l orange ff9e64      #ff9e64  index color 16
     set -l yellow e0af68      #e0af68  yellow
     set -l green 9ece6a       #9ece6a  green
     set -l purple 9d7cd8      #9d7cd8  dimmed "magenta" 
     set -l ltpurple bb9af7    #bb9af7  "magenta"
-    set -l azure 7dcfff       #7dcfff  bright "cyan"
-    set -l rasberry ab274f    #ab274f
+    set -l blue 7dcfff        #7dcfff
+    # set -l blue 20cfff        #20cfff
+    # ls | cat > foo
     set -l cyan 0cb4c0        #0cb4c0  cyan
 
     # Syntax highlighting colors
@@ -21,15 +22,15 @@ if not set -q tokyo_night_colors_set
     set -U fish_color_cancel -r
     set -U fish_color_cwd $green
     set -U fish_color_cwd_root $red
-    set -U fish_color_command $azure
+    set -U fish_color_command $blue
     set -U fish_color_comment $comment
     set -U fish_color_end $orange
     set -U fish_color_error $red
-    set -U fish_color_escape $rasberry
+    set -U fish_color_escape $red
     set -U fish_color_history_current --bold
-    set -U fish_color_host $rasberry
+    set -U fish_color_host $red
     set -U fish_color_host_remote $yellow
-    set -U fish_color_keyword $rasberry
+    set -U fish_color_keyword $ltpurple
     set -U fish_color_normal $foreground
     set -U fish_color_operator $green
     set -U fish_color_param $ltpurple
@@ -42,9 +43,9 @@ if not set -q tokyo_night_colors_set
     set -U fish_color_valid_path --underline
 
     # Completion pager colors
-    set -U fish_pager_color_completion $foreground
+    set >  -U fish_pager_color_completion $foreground
     set -U fish_pager_color_description $comment
-    set -U fish_pager_color_prefix $azure
+    set -U fish_pager_color_prefix $blue
     set -U fish_pager_color_progress $comment
     set -U fish_pager_color_selected_background -r
 
