209df04af053cefd65e998202ebc1f5a63450cfa
grscheller <geoffrey@scheller.com>
2022-07-28 16:48:52 -0600

    More fish TokyoNight colorscheme tweaking
     - Looks a little closer to nvim treesitter
       - probably can't get exactly
       - might want to compare with fish/shell LSP turned on?

--- config/fish/conf.d/colors.fish
+++ config/fish/conf.d/colors.fish
@@ -1,5 +1,8 @@
 ## Setup Tokyo Night based colors for fish to use
 
+# Examples: https://github.com/dmi3/fish/blob/master/colors.fish
+#           https://github.com/fish-shell/fish-shell/issues/3443
+
 if not set -q tokyo_night_colors_set
 
     # TokyoNight based colors consistent with my alacritty configuration
@@ -12,9 +15,7 @@ if not set -q tokyo_night_colors_set
     set -l green 9ece6a       #9ece6a  green
     set -l purple 9d7cd8      #9d7cd8  dimmed "magenta" 
     set -l ltpurple bb9af7    #bb9af7  "magenta"
-    set -l blue 7dcfff        #7dcfff
-    # set -l blue 20cfff        #20cfff
-    # ls | cat > foo
+    set -l azure 7dcfff       #7dcfff
     set -l cyan 0cb4c0        #0cb4c0  cyan
 
     # Syntax highlighting colors
@@ -22,20 +23,20 @@ if not set -q tokyo_night_colors_set
     set -U fish_color_cancel -r
     set -U fish_color_cwd $green
     set -U fish_color_cwd_root $red
-    set -U fish_color_command $blue
+    set -U fish_color_command $azure
     set -U fish_color_comment $comment
-    set -U fish_color_end $orange
-    set -U fish_color_error $red
+    set -U fish_color_end $azure
+    set -U fish_color_error --background=$red
     set -U fish_color_escape $red
     set -U fish_color_history_current --bold
     set -U fish_color_host $red
     set -U fish_color_host_remote $yellow
     set -U fish_color_keyword $ltpurple
     set -U fish_color_normal $foreground
-    set -U fish_color_operator $green
-    set -U fish_color_param $ltpurple
-    set -U fish_color_quote $yellow
-    set -U fish_color_redirection $foreground
+    set -U fish_color_operator $cyan
+    set -U fish_color_param $foreground
+    set -U fish_color_quote $green
+    set -U fish_color_redirection $azure
     set -U fish_color_search_match --background=$selection
     set -U fish_color_selection --background=$selection
     set -U fish_color_status $orange
@@ -45,7 +46,7 @@ if not set -q tokyo_night_colors_set
     # Completion pager colors
     set >  -U fish_pager_color_completion $foreground
     set -U fish_pager_color_description $comment
-    set -U fish_pager_color_prefix $blue
+    set -U fish_pager_color_prefix $azure
     set -U fish_pager_color_progress $comment
     set -U fish_pager_color_selected_background -r
 
