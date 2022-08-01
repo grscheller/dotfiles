c22608a7f6c04b5f7762d41614475d4905151b90
grscheller <geoffrey@scheller.com>
2022-07-31 18:44:27 -0600

    Neovim formating change packer.lua
     - should be no functionality changes (at least exernally) to configuration
     - all `use` statements now use brackets
       - indenting changes I made made everything look nicer
       - LSP autoformatting afterwards made things look even nicer
       - first time I am really happy with packer.lua indentation

--- config/nvim/lua/grs/util/packer.lua
+++ config/nvim/lua/grs/util/packer.lua
@@ -1,6 +1,6 @@
---[[ Using Packer as the plugin manager ]]
+--[[ Using Packer as the plugin manager
 
---[[ The Packer plugin manages the plugins I install as a Neovim Package.
+     Manages the plugins I install as a Neovim Package.
 
      This file configures the packer.nvim plugin which
      creates and manages the plugin package.  This package
@@ -17,8 +17,10 @@
 
        :PackerCompile
 
-     Not sure if necessary, but I would restart
-     Neovim after running either of the above commands.  ]]
+     Not always necessary, but I'd restart Neovim after
+     running either of the above commands.
+  ]]
+
 local ok, packer = pcall(require, 'packer')
 if not ok then
    print('Warning: Packer not installed/configured - no plugins, ')
@@ -43,32 +45,30 @@ local use = packer.use
 return packer.startup(
    function()
       --[[ Packer manages itself ]]
-      use 'wbthomason/packer.nvim'
+      use { 'wbthomason/packer.nvim' }
 
       --[[ Library used by other plugins ]]
-      use 'nvim-lua/plenary.nvim'
+      use { 'nvim-lua/plenary.nvim' }
 
       -- Make keybindings discoverable with Whick-Key
-      use 'folke/which-key.nvim'
+      use { 'folke/which-key.nvim' }
 
       --[[ General purpose text editing plugins ]]
-      use 'numToStr/Comment.nvim' -- comment out code - treesitter aware
-      use 'justinmk/vim-sneak' -- easy motion replacement
-      use 'tpope/vim-surround' -- surround text objects with matching symbols
-      use 'tpope/vim-repeat' -- enable supported plugins to use '.'
+      use { 'numToStr/Comment.nvim' } -- comment out code - treesitter aware
+      use { 'justinmk/vim-sneak' } -- easy motion replacement
+      use { 'tpope/vim-surround' } -- surround text objects with matching symbols
+      use { 'tpope/vim-repeat' } -- enable supported plugins to use `.` to repeat
 
       --[[ Colorscheme, statusline & zen-mode]]
-      use 'norcalli/nvim-colorizer.lua' -- Colorize names & hexcodes
-      use 'folke/tokyonight.nvim'
-      use {
-         'nvim-lualine/lualine.nvim',
+      use { 'norcalli/nvim-colorizer.lua' } -- Colorize names & hexcodes
+      use { 'folke/tokyonight.nvim' }
+      use { 'nvim-lualine/lualine.nvim',
          requires = {
             'kyazdani42/nvim-web-devicons',
             opt = true
          }
       }
-      use {
-         'folke/zen-mode.nvim',
+      use { 'folke/zen-mode.nvim',
          requires = { 'folke/twilight.nvim' }
       }
 
@@ -79,12 +79,11 @@ return packer.startup(
       }
 
       --[[ Telescope - highly extendable fuzzy finder over lists ]]
-      use 'nvim-telescope/telescope.nvim'
-      use 'nvim-telescope/telescope-ui-select.nvim'
+      use { 'nvim-telescope/telescope.nvim' }
+      use { 'nvim-telescope/telescope-ui-select.nvim' }
 
       --[[ Completion & snippet support ]]
-      use {
-         'hrsh7th/nvim-cmp',
+      use { 'hrsh7th/nvim-cmp',
          requires = {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-cmdline',
@@ -92,8 +91,7 @@ return packer.startup(
             'hrsh7th/cmp-nvim-lsp-signature-help',
             'hrsh7th/cmp-nvim-lua',
             'hrsh7th/cmp-path',
-            'lukas-reineke/cmp-rg',
-            {
+            'lukas-reineke/cmp-rg', {
                'saadparwaiz1/cmp_luasnip',
                requires = {
                   'L3MON4D3/LuaSnip',
@@ -104,15 +102,15 @@ return packer.startup(
       }
 
       --[[ DAP Debug Adapter Protocol ]]
-      use 'mfussenegger/nvim-dap'
+      use { 'mfussenegger/nvim-dap' }
 
       --[[ LSP Language Server Protocol ]]
-      use 'neovim/nvim-lspconfig'
-      use 'williamboman/nvim-lsp-installer'
-      use 'simrat39/rust-tools.nvim'
-      use 'scalameta/nvim-metals'
+      use { 'neovim/nvim-lspconfig' }
+      use { 'williamboman/nvim-lsp-installer' }
+      use { 'simrat39/rust-tools.nvim' }
+      use { 'scalameta/nvim-metals' }
 
       --[[ File detection/syntax highlighting for zig ]]
-      use 'ziglang/zig.vim'
+      use { 'ziglang/zig.vim' }
    end
 )
