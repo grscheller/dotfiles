e9f4b6c3bc07e30ff5dde317a30e8763ef8a1e64
grscheller <geoffrey@scheller.com>
2022-08-25 19:43:28 -0600

    Updated Neovim completions
     - file: ~/.config/nvim/lua/grs/completions.lua
     - simplified (flattened) source ordering

--- config/nvim/lua/grs/completions.lua
+++ config/nvim/lua/grs/completions.lua
@@ -72,13 +72,9 @@ cmp.setup {
       {
          { name = 'nvim_lsp' },
          { name = 'nvim_lsp_signature_help' },
-         { name = 'luasnip' }
-      },
-      {
+         { name = 'luasnip' },
+         { name = 'path' },
          { name = 'buffer' },
-         { name = 'path' }
-      },
-      {
          { name = 'rg',
            keyword_length = 5,
            max_item_count = 5,
