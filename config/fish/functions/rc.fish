4ad0175993e0ff40cd5b573e464551d0d5703d96
grscheller <geoffrey@scheller.com>
2022-09-22 05:47:20 -0600

    Updated ~/.config/fish/functions/rc.fish
     - change number of columns to match gauss17 external monitor

--- config/fish/functions/rc.fish
+++ config/fish/functions/rc.fish
@@ -1,3 +1,3 @@
 function rc --description 'Resize linux console for gauss17 external monitor'
-    test $TERM = linux; and stty rows 44 columns 169
+    test $TERM = linux; and stty rows 44 columns 158
 end
