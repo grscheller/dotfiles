bb604f74b283cdffec3dda64db0dfc651d5137b9
grscheller <geoffrey@scheller.com>
2022-10-22 09:55:06 -0600

    Renamed Neovim config file
     - completions_snippets.lua -> completions.lua
     - adjustments for name change
     - no other changes

--- home/config/nvim/lua/grs/devel/init.lua
+++ home/config/nvim/lua/grs/devel/init.lua
@@ -1,5 +1,5 @@
 --[[ Configure an IDE like LSP based software development environment ]]
 
 require('grs.devel.telescope')
-require('grs.devel.completions_snippets')
+require('grs.devel.completions')
 require('grs.devel.lsp')
