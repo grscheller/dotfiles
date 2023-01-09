9b220c241217ade5b574facd9c2216fb315715a7
grscheller <geoffrey@scheller.com>
2023-01-09 11:13:00 -0500

    Neovim housekeeping
     - lua/grs/plugin/ -> lua/grs/plugins/
       - keep things more or less consistent with LazyVim/LazyVim

--- home/config/nvim/lua/grs/config/lazy.lua
+++ home/config/nvim/lua/grs/config/lazy.lua
@@ -16,7 +16,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
-   spec = 'grs.plugin',
+   spec = 'grs.plugins',
    defaults = { lazy = true, version = '*' },
    install = { colorscheme = { 'kanagawa' } },
    checker = { enabled = true }, -- check for plugin updates
