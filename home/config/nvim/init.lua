8ff2991cdb62f4e5d1a09df7499bb260958ca3f1
grscheller <geoffrey@scheller.com>
2023-01-18 00:01:29 -0500

    REVERT: mapleader & maplocalleader from last commit
     - <leader> & <localleader> were not being picked up
     - had to move vim.g.mapleader & vim.g.maplocalleader back to init.lua

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -1,3 +1,6 @@
 --[[ GRS Neovim Configuration - transitioning to lazy.nvim ]]
 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require('grs.config.lazy')
--- home/config/nvim/lua/grs/config/globals.lua
+++ home/config/nvim/lua/grs/config/globals.lua
@@ -1,7 +1,4 @@
 --[[ Globals - most plugins only read these on startup ]]
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 vim.g.python3_host_prog =
    string.format('%s/.local/share/pyenv/shims/python', os.getenv 'HOME')
