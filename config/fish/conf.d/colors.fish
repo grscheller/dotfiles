7fda431f9e2dba2d4886131d94a46626248799ab
grscheller <geoffrey@scheller.com>
2023-04-13 21:17:52 -0600

    Finished fishcolors.fish
     - also added comments to config/fish/conf.d/colors.fish
     - on previous commit, changed ~/.alacritty/alacritty.yml
       - changed draw_bold_text_with_bright_colors to false
       - this is what was making fish colors change

--- config/fish/conf.d/colors.fish
+++ config/fish/conf.d/colors.fish
@@ -3,7 +3,6 @@
 # Examples: https://github.com/dmi3/fish/blob/master/colors.fish
 #           https://github.com/fish-shell/fish-shell/issues/3443
 
-
     # Kanagawa inspired color pallet
 set -l foreground dcd7ba  #dcd7ba
 set -l selection 33467c   #33467c
@@ -18,30 +17,27 @@ set -l cyan 7aa89f        #7aa89f
 set -l pink d27e99        #d27e99
 
 # Syntax highlightin  colors
-set -U fish_color_normal $foreground
-set -U fish_color_command $cyan
-set -U fish_color_keyword $pink
-set -U fish_color_quote $yellow
-set -U fish_color_redirection $blue
-set -U fish_color_end $orange
-set -U fish_color_error $red
-set -U fish_color_param $purple
-set -U fish_color_comment $comment
-set -U fish_color_selection --background=$selection
-set -U fish_color_search_match --background=$selection
-set -U fish_color_operator $green
-set -U fish_color_escape $pink
-set -U fish_color_autosuggestion $comment
-set -U fish_color_cancel -r
-set -U fish_color_cwd $blue
-set -U fish_color_history_current --bold
-set -U fish_color_status $orange
-set -U fish_color_user $cyan
-set -U fish_color_valid_path --underline
+set -U fish_color_normal $foreground      # default color
+set -U fish_color_command $cyan           # color for commands
+set -U fish_color_keyword $pink           # fish keywords
+set -U fish_color_quote $yellow           # quoted text
+set -U fish_color_redirection $blue       # IO redirections
+set -U fish_color_end $orange             # process separators like ; or & or |
+set -U fish_color_error $red              # syntax errors
+set -U fish_color_param $purple           # ordiary command parameters
+set -U fish_color_comment $comment        # code comments
+set -U fish_color_operator $green         # parameter expansion operators like '*' and '~'
+set -U fish_color_escape $pink            # character escapes like '\n' and '\x70'
+set -U fish_color_autosuggestion $comment # proposed rest of a command
+set -U fish_color_cancel $orange          # CTRL-C indicator
+set -U fish_color_user $cyan              # username in default prompt
+set -U fish_color_valid_path --underline  # valid path names
+set -U fish_color_selection --background=$selection    # selected text in vi visual mode
+set -U fish_color_search_match --background=$selection # highlight history search matches
 
 # Completion pager colors
 set -U fish_pager_color_progress $comment
 set -U fish_pager_color_prefix $cyan
 set -U fish_pager_color_completion $foreground
-set -U fish_pager_color_description $comment
+set -U fish_pager_color_description $blue
 set -U fish_pager_color_selected_background -r
--- config/fish/functions/fishcolors.fish
+++ config/fish/functions/fishcolors.fish
@@ -1,6 +1,16 @@
 function fishcolors --description 'Show colors used by fish'
-   set -l color_vars (set -n|grep 'fish.*color'|grep -v '^_')
-   for var in $color_vars
-      printf '| %-40s | %-40s |\n' $var $$var
+   set -l color
+   set -l color_def
+   set -l normal (set_color normal)
+   set -l format '| %s%-38s%s | %-22s |\n'
+   printf '___________________________________________________________________\n'
+   for color_var in (set -n|grep 'fish.*color'|grep -v '^_')
+      set color_def $$color_var
+      if not set color (set_color $color_def 2>/dev/null)
+         set color_def 'ERROR!!!'
+         set color (set_color normal)
+      end
+      printf $format $color $color_var $normal $color_def
    end
+   printf '¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n'
 end
