4da2e152d3374bf2e8261b2111dd1677289b927f
grscheller <geoffrey@scheller.com>
2023-04-25 07:29:19 -0600

    Continue LSP/completions Neovim reconfiguration
     - vastly simplified Scala Metals configuration
       - previously was not working correctly
     - tweaked Rust-Tools configuration

--- config/nvim/lua/grs/plugins/lsp/init.lua
+++ config/nvim/lua/grs/plugins/lsp/init.lua
@@ -116,10 +116,11 @@ return {
       end,
    },
 
-   -- Rust-Tools directly configures lspconfig for rust-analyzer itself
+   -- Rust-Tools itself directly configures rust-analyzer with nvim-lspconfig 
    --
-   -- Initially followed both https://github.com/simrat39/rust-tools.nvim
-   -- and https://github.com/sharksforarms/neovim-rust.
+   --    Original setup based on:
+   --      https://github.com/simrat39/rust-tools.nvim
+   --      https://github.com/sharksforarms/neovim-rust
    --
    -- Todo: see https://davelage.com/posts/nvim-dap-getting-started/
    --       and :help dap-widgets
@@ -133,7 +134,6 @@ return {
          'nvim-lua/plenary.nvim',
          'nvim-telescope/telescope.nvim',
          'neovim/nvim-lspconfig',
-         'j-hui/fidget.nvim',
       },
       enabled = LspTbl.system.rust_tools == m.man,
       ft = { 'rust' },
@@ -186,39 +186,37 @@ return {
       end
    },
 
-   -- Scala Metals directly configures lspconfig itself
+   -- Scala Metals directly configures the Neovim LSP client itself
+   -- and does not involke nvim-lspconfig at all.
+   --
    --    Latest Metals Server: https://scalameta.org/metals/docs
-   --    Following: https://github.com/scalameta/nvim-metals/discussions/39
-   --               https://github.com/scalameta/nvim-metals/discussions/279
+   --
+   --    Original setup based on:
+   --      https://github.com/scalameta/nvim-metals/discussions/39
+   --      https://github.com/scalameta/nvim-metals/discussions/279
+   -- 
    {
       'scalameta/nvim-metals',
       dependencies = {
-         'hrsh7th/cmp-nvim-lsp',
-         'mfussenegger/nvim-dap',
-         'neovim/nvim-lspconfig',
          'nvim-lua/plenary.nvim',
-         'j-hui/fidget.nvim',
+         'mfussenegger/nvim-dap',
       },
       enabled = LspTbl.system.scala_metals == m.man,
+      -- Don't trigger this config for java files, but once triggered
+      -- metals will take control over java code.
       ft = { 'scala', 'sbt' },
-      init = function()
-         autocmd('FileType', {
-            pattern = { 'scala', 'sbt' },
-            callback = function()
-               grs_metals.metals.initialize_or_attach(grs_metals.config)
-            end,
-            group = autogrp('GrsMetals', { clear = true }),
-         })
-      end,
       config = function()
-         grs_metals.metals = require 'metals'
-         grs_metals.config = grs_metals.metals.bare_config()
-         grs_metals.config.settings = {
+         local metals = require 'metals'
+         local metals_config = metals.bare_config()
+         metals_config.settings = {
             showImplicitArguments = true,
-            serverVersion = '0.11.11',
+            excludedPackages = {
+               "akka.actor.typed.javadsl",
+               "com.github.swagger.akka.javadsl",
+            },
          }
-         grs_metals.config.capabilities = require('cmp_nvim_lsp').default_capabilities()
-         grs_metals.config.init_options.statusBarProvider = 'on'
+         metals_config.capabilities = require('cmp_nvim_lsp').default_capabilities()
+         metals_config.init_options.statusBarProvider = 'on'
 
          local dap = require 'dap'
          local dap_ui_widgets = require 'dap.ui.widgets'
@@ -241,28 +239,38 @@ return {
                },
             },
          }
-         function grs_metals.config.on_attach(_, bufnr)
+
+         metals_config.on_attach = function(_, bufnr)
             -- set up dap
-            grs_metals.metals.setup_dap()
+             metals.setup_dap()
 
             -- set up keymaps
             km.lsp(bufnr)
             km.metals(bufnr, grs_metals.metals)
             km.dap(bufnr, dap, dap_ui_widgets)
 
-            -- show diagnostic popup when cursor lingers on line with errors
-            autocmd('CursorHold', {
-               buffer = bufnr,
+            local GrsMetalsGrp = autogrp('GrsMetals', { clear = true })
+            autocmd('FileType', {
+               pattern = { 'scala', 'sbt', 'java' },
                callback = function()
-                  vim.diagnostic.open_float {
-                     bufnr = bufnr,
-                     scope = 'line',
-                     focusable = false,
-                  }
+                  metals.initialize_or_attach(metals_config)
                end,
-               group = autogrp('GrsMetals', { clear = false }),
-               desc = 'Open floating diagnostic window for Scala-Metals',
+               group = GrsMetalsGrp,
             })
+            --function grs_metals.config.on_attach(_, bufnr)
+               -- -- show diagnostic popup when cursor lingers on line with errors
+               -- autocmd('CursorHold', {
+               --    buffer = bufnr,
+               --    callback = function()
+               --       vim.diagnostic.open_float {
+               --          bufnr = bufnr,
+               --          scope = 'line',
+               --          focusable = false,
+               --       }
+               --    end,
+               --    group = autogrp('GrsMetals', { clear = false }),
+               --    desc = 'Open floating diagnostic window for Scala-Metals',
+               -- })
          end
       end,
    },
