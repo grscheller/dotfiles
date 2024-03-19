4fd83eb6ecd57bc8ac3a4df01b69e339b4fae776
grscheller <geoffrey@scheller.com>
2024-03-19 09:34:27 -0600

    Now all $XDG_*_HOME vars handled by bin/.setuo.sh
      XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=$HOME/.config}"
      XDG_CACHE_HOME="${XDG_CACHE_HOME:=$HOME/.cache}"
      XDG_DATA_HOME="${XDG_DATA_HOME:=$HOME/.local/share}"
      XDG_STATE_HOME="${XDG_STATE_HOME:=$HOME/.local/state}"
    
    - not just the ones I previously dealt with
      - added XDG_DATA_HOME
      - future proofing
    - no need to export them
      - if seen, they are already exported
      - if not seen, they default to right place
      - my scripts just need to know what they are

--- bin/.setup.sh
+++ bin/.setup.sh
@@ -22,8 +22,9 @@ if [ -z "$dfOption" ]
 then
    umask 0022
    XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=$HOME/.config}"
-   XDG_STATE_HOME="${XDG_STATE_HOME:=$HOME/.local/state}"
    XDG_CACHE_HOME="${XDG_CACHE_HOME:=$HOME/.cache}"
+   XDG_DATA_HOME="${XDG_DATA_HOME:=$HOME/.local/share}"
+   XDG_STATE_HOME="${XDG_STATE_HOME:=$HOME/.local/state}"
 
    ## Parse cmdline arguments
 
