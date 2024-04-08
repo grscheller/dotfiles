05ff027da0406cdc6de64518c65c8370bb28d0ec
grscheller <geoffrey@scheller.com>
2024-04-08 06:17:39 -0600

    Refactoring fish function ve
    - Work in progress
    - performs more checks
    - will clear and redo Python virtual environments
      - replaces fish abbreviations
    - still switches between virtual envs (still needs more testing)

--- fish-env/config/fish/functions/ve.fish
+++ fish-env/config/fish/functions/ve.fish
@@ -1,56 +1,113 @@
-## Switch Python virtual environments
-#
-# Even though Python virtual environments know the location of the version
-# of Python they need, My Neovim configuration needs access to Python tools
-# installed into a Python's virtual environment. Usually due to the tool not
-# being managed by the Linux distribution's package manager system.
-#
-# To simply my Neovim configuration, the Lua variable vim.g.python3_host_prog
-# is set to the pyenv managed Python ~/.local/share/pyenv/shims/python and no
-# attempt is made to 
-#
-# As a result, I need to ensure the vir
-#
-function ve --description 'Switch Python Virtual environments'
-   # Sanity check and setup if needed
+function ve --description 'Instantiate or configure a Python virtual env'
+
+   # Provide override mechanism to usual virtual env location
    if not set -q PYTHON_GRS_ENVS
-      set PYTHON_GRS_ENVS ~/devel/python_envs/
+      set -g PYTHON_GRS_ENVS ~/devel/python_envs/
    end
    test -d $PYTHON_GRS_ENVS || mkdir -p $PYTHON_GRS_ENVS
 
-   # argment parsing
-   set veName $argv
-   if test (count $veName) -ge 1
-      set veName $veName[1]
-      switch $veName
-      case grs devel jupyter_learn neovim pypy py4ai
-         :
-      case 'pypi*'
-         set veName pypi3_12_1
-      case '*'
-         set fmt 'Warning: Untracked Python virtual environment: %s\n'
-         printf $fmt $argv[1] >&2
-      end
-   else
-      set veName ''
-   end
+   # Parse commandline
+   set argc (count $argv)
+   set arg1 $argv[1]
 
-   if test -n $veName && not test -e $PYTHON_GRS_ENVS/$veName/bin/activate.fish
-      set fmt 'Info: No venv "%s" found in $PYTHON_GRS_ENVS: %s\n'
-      printf $fmt $veName "$PYTHON_GRS_ENVS" >&2
+   if test $argc -gt 1 -o $arg1 = '-h' -o $arg1 = '--help'
+      printf 'Error: invalid arguments\n\n'
+      printf 'Usage: ve [veName]\n'
+      printf '       ve [-c | --clear]\n'
+      printf '       ve [-r | --redo]\n'
+      printf '       ve [-h | --help]\n\n'
       return 1
    end
 
-   # Deactivate Python virtual environment if no arguments given
-   if test -z $veName[1]
+   if test $argc -eq 0
+      # deactivate current virtual env if we are in one
       type -q deactivate && deactivate
-      set fmt 'No Python venv in use, using %s\n'
+      set fmt 'No Python venv in use, using python version %s\n'
       printf $fmt (python --version)
       return 0
    end
 
-   # Activate Python virtual environment
-   source $PYTHON_GRS_ENVS/$veName/bin/activate.fish
+   set -e optRedo optClear
+   switch $argv[1]
+      case -c --clear 
+         set optClear
+      case -r --redo
+         set optRedo
+      case '*'
+         set veName $arg1
+   end
 
-   return 0
+   # Semantic parsing - edit here for new managed environments
+   set -e pythonVersion python_env
+   if set -q optClear || set -q optRedo
+      :
+   else
+      switch $veName
+      case grs devel jupyter_learn neovim pypy py4ai
+         set python_env $PYTHON_GRS_ENVS/$veName
+         set pythonVersion '3.11.8'             # current arch system python
+      case devNext pypi3_12_1                                        # tests
+         set python_env $PYTHON_GRS_ENVS/$veName
+         set pythonVersion '3.12.2'
+      case '*'
+         set fmt 'Warning: Untracked Python virtual environment: %s\n'
+         printf $fmt $argv[1] >&2
+         set python_env $PYTHON_GRS_ENVS/$veName
+      end
+
+      # Check if virtual env actually exists
+      if test -n $veName && not test -e $python_env/bin/activate.fish
+         set fmt 'Info: No venv "%s" found in $PYTHON_GRS_ENVS: %s\n'
+         printf $fmt $veName $PYTHON_GRS_ENVS >&2
+         return 1
+      end
+
+      # Activate Python virtual environment
+      set -gx PYTHON_GRS_ENV $python_env
+      source $PYTHON_GRS_ENV/bin/activate.fish
+      return 0
+   end
+
+   ## Manage the current virtual environment
+
+   # Sanity checks
+   if not set -q PYTHON_GRS_ENV
+      printf 'Not in a ve managed Python virtual environment.\n'
+      return 1
+   end
+
+   if digpath -q python
+      set pyVers (string split -f 2 ' ' (python --version))
+   else
+      set msg 'Python executable not found, yet $PYTHON_GRS_ENV = %s\n'
+      printf $msg $PYTHON_GRS_ENV
+      return 1
+   end
+
+   set pythonPath (which python)
+   set pythonVe $PYTHON_GRS_ENV/bin/python
+   if test $pythonPath = $pythonVe
+      set pythonVer pyVers[1]
+      set pypyVer pyVers[2]
+      set veName (string replace -r '^.*/' '' $PYTHON_GRS_ENV)
+   else
+      set msg 'Python found on $PATH, %s, is not %s./n'
+      printf $msg $pythonPath $PythonVe
+      return 1
+   end
+
+   # Consistency checks
+
+   # Parse commandline arguments
+   set opt $argv[1]
+   switch $opt
+      case -c --clear
+         printf 'option %s given\n' $opt
+      case -r --redo
+         printf 'option %s given\n' $opt
+      case '*'
+         printf 'Unknown option: %s\n' $opt
+         return 1
+   end
+         
 end
