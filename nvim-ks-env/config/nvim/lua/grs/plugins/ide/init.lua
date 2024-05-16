016ac927287a3eb72a842d25cbde80db54b31e5a
grscheller <geoffrey@scheller.com>
2024-05-16 15:46:54 -0600

    Renamed plugins/lsp/ -> plugins/ide.lua
    - incorporated debug.lua into ide/lsp.lua
    
    TODO:
    - some more reorganization is needed before setting lazy = true
    - will move plugins/autopairs.lua into plugins/ide/cmp.lua

---
+++ nvim-ks-env/config/nvim/lua/grs/plugins/ide/init.lua
@@ -0,0 +1,10 @@
+--[[ Configure LSP, DAP, plugins to make Neovim into an IDE ]]
+
+local iFlatten = require('grs.lib.functional').iFlatten
+
+return iFlatten {
+   require 'grs.plugins.ide.cmp',
+   require 'grs.plugins.ide.lsp',
+   -- require 'grs.plugins.ide.rust',
+   -- require 'grs.plugins.ide.scala',
+}
