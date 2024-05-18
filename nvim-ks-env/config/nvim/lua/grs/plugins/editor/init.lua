c3ed4a372a6b171e32ab0dbfedc2ba7d98b23396
grscheller <geoffrey@scheller.com>
2024-05-18 12:21:17 -0600

    Moved plugins/editor/{colorscheme -> appearance}.lua
    - also fixed bungled kyazdani42/nvim-web-devicons configuration

--- nvim-ks-env/config/nvim/lua/grs/plugins/editor/init.lua
+++ nvim-ks-env/config/nvim/lua/grs/plugins/editor/init.lua
@@ -4,19 +4,22 @@ local iFlatten = require('grs.lib.functional').iFlatten
 
 return iFlatten {
 
-   -- Nvim-web-devicons needs patched a font.
-   -- See https://github.com/ryanoasis/nerd-fonts
    {
-      'kyazdani42/nvim-web-devicons',
-      enabled = vim.g.have_nerd_font,
-      opts = {
-         color_icons = true,
-         default = true,
-         strict = true,
+      -- Nvim-web-devicons needs patched a font.
+      -- See https://github.com/ryanoasis/nerd-fonts
+      {
+         'kyazdani42/nvim-web-devicons',
+         enabled = vim.g.have_nerd_font,
+         opts = {
+            color_icons = true,
+            default = true,
+            strict = true,
+         },
       },
+
    },
 
-   require 'grs.plugins.editor.colorscheme',
+   require 'grs.plugins.editor.appearance',
    -- require 'grs.plugins.editor.telescope',
    require 'grs.plugins.editor.textedit',
    -- require 'grs.plugins.editor.whichkey',
