118e0b77426790d132d0d738494194adb06de2e3
grscheller <geoffrey@scheller.com>
2024-05-23 13:58:02 -0600

    Fixed, code-based in a state of flux
    - mason-lspconfig is where LSP "handlers" should be defined

--- nvim-env/config/nvim/lua/grs/plugins/ide/lsp.lua
+++ nvim-env/config/nvim/lua/grs/plugins/ide/lsp.lua
@@ -30,7 +30,8 @@ return {
          require('mason').setup {}
          require('mason-lspconfig').setup {
             ensure_installed = {},
-
+            automatic_installation = true,
+            handlers = nil,
          }
 
          -- setup before configuring any LSP servers with lspconfig
