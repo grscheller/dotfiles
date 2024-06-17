97bcdb13c7fcde158f97992c8f98f6188e90d34b
grscheller <geoffrey@scheller.com>
2024-06-17 09:23:34 -0600

    Repeating changes made over weekend
    - forgot to push from home computer, will merge when I get home
    - will use Python 3.10.13 for Udacity ai course (venv with flit)
    - will actually develop with Python 3.12.3 (latest Arch system python)

--- fish-env/completions/ve.fish
+++ fish-env/completions/ve.fish
@@ -6,17 +6,11 @@ complete -c ve \
   -n 'not __fish_seen_subcommand_from dev' \
   -a 'dev'
 complete -c ve \
-  -n 'not __fish_seen_subcommand_from dev_next' \
-  -a 'dev'
-complete -c ve \
   -n 'not __fish_seen_subcommand_from grs' \
   -a 'grs'
 complete -c ve \
   -n 'not __fish_seen_subcommand_from py4ai' \
   -a 'py4ai'
 complete -c ve \
-  -n 'not __fish_seen_subcommand_from pydev' \
-  -a 'pydev'
-complete -c ve \
-  -n 'not __fish_seen_subcommand_from pydev_next' \
-  -a 'pydev_next'
+  -n 'not __fish_seen_subcommand_from py4ai_old' \
+  -a 'py4ai_old'
