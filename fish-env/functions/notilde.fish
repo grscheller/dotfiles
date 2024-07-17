9335a3c053768bb8986c772384188a9bdab10a24
grscheller <geoffrey@scheller.com>
2024-07-17 08:26:39 -0600

    Renamed fish-env/functions/no{tilda -> tilde}.fish
    - sometimes I spell like  a fifth grader
    - updated nvim-env/spell/en.utf-8.add

--- fish-env/functions/notilde.fish
+++ fish-env/functions/notilde.fish
@@ -1,4 +1,4 @@
-function notilda --description 'strip pdoc3 generated tildas from TypeVars'
+function notilde --description 'strip pdoc3 generated tildes from TypeVars'
    for File in (fd --type file --extension html)
       sed -E 's/~_([A-Z])/_\1/g' $File > {$File}_temp
       mv {$File}_temp {$File}
