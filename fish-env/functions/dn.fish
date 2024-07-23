2c2bdf359bbdcec7d1a7edab03d5c99543e41120
grscheller <geoffrey@scheller.com>
2024-07-23 12:35:31 -0600

    Rewrote .config/fish/functions/dn.fish
    - no longer uses fd

--- fish-env/functions/dn.fish
+++ fish-env/functions/dn.fish
@@ -1,50 +1,22 @@
 function dn --description 'Jump down directory tree to find an item'
-   set -f firstTarget
-   set -f firstDir
 
-   set -f argc (count $argv)
-
-   if test (count $argv) -gt 1 
-      printf 'dn: dn only takes 0 or 1 arguments' >&2
-      return 2
-   end
-
-   if test "$argc" -eq 0
-      # Go to first directory dn finds
-      set firstDir (fd --type directory --max-results=1) ''
-      if test -n "$firstDir[1]"
-         cd $firstDir[1]
-         return $status
-      else
-         printf 'Nowhere to drop down to!\n' >&2
-         return 1
-      end
-   end
-
-   set -f fdArgs --glob --unrestricted --case-sensitive --max-results=1
-   set -f pattern "$argv[1]"
-   # First look for first exact match.
-   set firstTarget (fd $fdArgs $pattern)
-   # Next look for leading string match.
-   set -a firstTarget (fd $fdArgs $pattern'*')
-   # Finally look for containing string match.
-   set -a firstTarget (fd $fdArgs '*'$pattern'*')
-
-   if test -z "$firstTarget[1]"
-      printf 'dn: pattern "*%s*" not found for any lower directory\n' $pattern >&2
+   if test (count $argv) -ne 1 
+      printf 'dn: dn only takes exactly one argument' >&2
       return 1
    end
 
-   if test -d "$firstTarget[1]"
-      set firstDir $firstTarget[1]
-   else
-      set firstDir (dirname $firstTarget[1])
+   set -l pat $argv[1]
+   set -l found **/$pat*
+   set -l cnt (count $found)
+   if test "$cnt" -eq 0
+      printf 'dn: pattern "%s" not found in any subdirectory' $pat >&2
+      return 1
    end
 
-   if cd $firstDir 2>/dev/null
-      return 0
+   set target $found[1]
+   if test -d "$target"
+      cd $target
    else
-      printf 'dn: failed to cd to "%s"\n    for pattern "%s"\n' $destination $target
-      return 3
+      cd (dirname $target)
    end
 end
