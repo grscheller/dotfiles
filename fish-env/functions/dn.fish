3d9f6c28a88cd7ee418cd36b530861b1eb648210
grscheller <geoffrey@scheller.com>
2024-08-06 11:16:22 -0600

    Modified functions/dn.fish fish function
    - "dn pattern"
      - first checks in current directory for pattern*
        - jumps to first one that is a directory
      - then checks in current directory for *pattern*
        - jumps to first one that is a directory
      - then searches first for **/pattern*
        - jumps to last one if a director or the directory containing it
      - then searches first for **/*pattern*
        - jumps to last one if a director or the directory containing it
      - why last? ** globs seem to be using a depth-first search algorithm

--- fish-env/functions/dn.fish
+++ fish-env/functions/dn.fish
@@ -1,11 +1,30 @@
 function dn --description 'Jump down directory tree to find an item'
 
    if test (count $argv) -ne 1 
-      printf 'dn: dn only takes exactly one argument' >&2
+      printf 'dn: dn takes exactly one argument' >&2
       return 1
    end
 
    set -l pat $argv[1]
+
+   # Jump to first target in current directory which *pat or *pat* matches
+   set -l targets $pat*
+   for target in $targets
+      if test -d "$target"
+         cd $target
+         return 0
+      end
+   end
+
+   set -l targets *$pat*
+   for target in $targets
+      if test -d "$target"
+         cd $target
+         return 0
+      end
+   end
+
+   # Now lets dig down some more
    set -l found **/$pat
    set -l cnt (count $found)
    if test "$cnt" -eq 0
@@ -21,7 +40,8 @@ function dn --description 'Jump down directory tree to find an item'
       end
    end
 
-   set target $found[1]
+   # Jump to last target found by the glob (will tend to be less deep)
+   set target $found[-1]
    if test -d "$target"
       cd $target
    else
