77df37cb1ea98ba355084108860a8f203bf779ee
grscheller <geoffrey@scheller.com>
2024-08-18 06:05:52 -0600

    Nvim tweaks
    - spell file
    - after/ftplugin/html.lus

--- nvim-env/after/ftplugin/html.lua
+++ nvim-env/after/ftplugin/html.lua
@@ -4,3 +4,5 @@ vim.opt_local.softtabstop = 2
 vim.opt_local.expandtab = true
 vim.opt_local.autoindent = true
 vim.opt_local.shiftround = true
+vim.opt_local.textwidth = 120
+vim.opt_local.colorcolumn = '+1,+21'
