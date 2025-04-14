d11ab3dc4b59cb15d01d6fc466c8e9441c1a2b68
grscheller <geoffrey@scheller.com>
2025-04-14 10:36:53 -0600

    BROKEN: functions/ud.fish
    - in the middle of refactoring it

--- fish-env/functions/ud.fish
+++ fish-env/functions/ud.fish
@@ -1,85 +1,117 @@
 function ud --description 'Jump up multiple directories, default is 1'
-   set -f upDir ..
-   set -f maxUp (math (string split / (pwd) | count) - 1)
-   set -f nDirs
-   set -f target ''
-
-   set -f argc (count $argv)
-
-   if test (count $argv) -gt 1 
+   if test (count $argv) -gt 1
       printf 'ud: ud only takes 0 or 1 arguments\n\n'
       return 2
    end
 
-   if test $argc -eq 0
+   # Pop up one directory if no arguments, or an empty string, was given
+   if test (count $argv) -eq 0 || test -z $argv[1]
       cd ..
       return $status
    end
-      
-   if string match -qr -- '^[1-9]\d*$' $argv[1]
-      # If argument is a number, jump up that number of directories
-      set nDirs $argv[1]
-      test $nDirs -gt $maxUp && set nDirs $maxUp
-   else
-      # Otherwise set nDirs to $maxUp and set target to first argument.
-      set nDirs $maxUp
-      set target $argv[1]
+
+   set -f target $argv[1]
+   set -f cnt 1
+   set -f upDir ..
+   set -f maxUp (math (string split / (pwd) | count) - 1)
+
+   # If the target given was a number, jump up that number of directories
+   if string match -qr -- '^[1-9]\d*$' $target
+      set maxUp (math "min($target, $maxUp)")
+      while test $cnt -lt $maxUp
+         set upDir ../$upDir
+         set cnt (math $cnt + 1)
+      end
+      cd $upDir
+      return $status
    end
 
-   set -f cnt 1
-   if test -z "$target"
-      # Setup path of directories to jump if no target given
-      while test $cnt -lt $nDirs
-         set upDir ../$upDir
-         set cnt (math $cnt + 1)
-      end
-   else
-      # First look for exact match
-      while test $cnt -le $nDirs
-         if test -e "$upDir/$target"
-            set -f _found_it_42
-            if not string match -q .. $upDir
-                break
+   set -f targetsFound
+   set -f targetFound ""
+   set -f baseName (basename (pwd))
+
+   # First look for an exact match
+   while test $cnt -le $maxUp
+      if test -e $upDir/$target
+         if test "$upDir" = ".."
+            if test "$target" = "$baseName"
+               # don't find current directory
+               set upDir ../$upDir
+               set cnt (math $cnt + 1)
+               continue
             end
          end
-         set upDir ../$upDir
-         set cnt (math $cnt + 1)
+         set targetFound $upDir/$target
+         echo got to 3: $targetFound
+         break
       end
-      if not set -q _found_it_42
-         # Next look for leading string match, select first one found.
-         set cnt 1
-         set upDir ..
-         set -f targetsFound
-         set -f upDirPat
-         while test $cnt -le $maxUp
-            if test (count $upDir/$target*) -gt 0
-               set targetsFound $upDir/$target*
-               set target $targetsFound[1]
-               set upDirPat (string replace -a '.' '\\.' $upDir)
-               set target (string replace -r $upDirPat/ '' $target)
+      set upDir ../$upDir
+      set cnt (math $cnt + 1)
+   end
+
+   echo got to 4: $targetFound
+
+   # Next check in case a quoted glob pattern was given
+   if test -z $targetFound
+      set cnt 1
+      set upDir ..
+      while test $cnt -le $maxUp
+         eval set targetsFound $upDir/$target
+         if test (count $targetsFound) -gt 0
+            if test (count $targetsFound) -eq 1
+               set targetFound $targetsFound
                break
             end
-            set upDir ../$upDir
-            set cnt (math $cnt + 1)
+            # for multiple matches, randomly select one
+            set targets (string replace -r ^(string repeat -n (math "2 + 3*($cnt - 1)") .) "" $targetsFound)
+            set target $targets[(random 1 (count $targets))]
+            break
          end
+         set upDir ../$upDir
+         set cnt (math $cnt + 1)
       end
    end
 
-   set -f destination
-   set -f targetDest $upDir/$target
-   if test ! -e "$targetDest"
-      printf 'ud: "%s" not found above current directory\n\n' $target
-      return 1
-   else if test -d "$targetDest"
-      set destination $targetDest
-   else
-      set destination (dirname $targetDest)
+   echo got to 5: $targetFound
+
+   # Finally check if an initial pattern was given
+   if test -z $targetFound
+      set cnt 1
+      set upDir ..
+      while test $cnt -le $maxUp
+         eval set targetsFound $upDir/$target*
+         if test (count $targetsFound) -gt 0
+            if test (count $targetsFound) -eq 1
+               set targetFound $targetsFound
+               break
+            end
+            # for multiple matches, randomly select one
+            set targets (string replace -r ^(string repeat -n (math "2 + 3*($cnt - 1)") .) "" $targetsFound)
+            set target $targets[(random 1 (count $targets))]
+            break
+         end
+         set upDir ../$upDir
+         set cnt (math $cnt + 1)
+      end
    end
 
-   if cd $destination 2>/dev/null
-      return 0
+   echo got to 6: $targetFound
+
+   set -f destination
+   if test -z "$targetFound"
+      printf 'ud: "%s" not found above current directory\n\n' $target
+      return 1
+   else if test -d "$targetFound"
+      set destination $targetFound
    else
-      printf 'ud: failed to cd to "%s"\n    for target "%s"\n\n' $destination $target
-      return 3
+      set destination (dirname $targetFound)
    end
+
+   echo got to 7: $targetFound
+
+   if cd $destination[1] 2>/dev/null
+      return 0
+   end
+   printf 'ud: failed to cd to "%s"\n    for target "%s"\n\n' $destination $target
+   return 3
 end
