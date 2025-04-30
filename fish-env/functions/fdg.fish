6636150a8bb6ed92cca4e149ea4827482e5e6713
grscheller <geoffrey@scheller.com>
2025-04-30 10:42:22 -0600

    Consolidated fdgf.fish and fdgp.fish to fdg.fish
    - $ fdg pull  # to pull all repos
    - $ fdg push  # to push all repos
    - $ fdg       # to fetch all repos (the default for any other args)

---
+++ fish-env/functions/fdg.fish
@@ -0,0 +1,37 @@
+function fdg --description 'Update GIT repos in subdirectories'
+   if test -d ./.git
+      printf '\nPunting... In a git working directory!\n'
+   else
+      switch $argv[1]
+      case 'pull'
+         set action pull
+      case 'push'
+         set action push
+      case '*'
+         set action fetch
+      end
+      set -l brblue (set_color brblue)
+      set -l normal (set_color normal)
+
+      set gitDirs (fd --hidden --no-ignore --type directory '^.git$')
+      set repos ()
+
+      string replace -r '/\.git/$' '' $gitDirs | while read line
+         set repos $repos $line
+      end
+
+      for repo in $repos
+         cd "$repo"
+         set branch (git branch --show-current)
+         begin
+            printf '\n%s%s:%s\n' $brblue $repo $normal
+            git --no-pager $action | sed -e 's/^/   /'
+            printf '   '
+            git --no-pager status --short --branch
+            git --no-pager diff --stat --color origin/$branch | sed -e 's/^/  /'
+         end
+         cd -
+      end
+   end
+   printf '\n'
+end
