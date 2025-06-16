4f22ce313e3a59003834a9098705c0e28bda3908
grscheller <geoffrey@scheller.com>
2025-06-16 00:06:25 -0600

    Got LuaRocks kinda working
    - it pulls from the nvim-neorocks Lux repo
      - https://nvim-neorocks.github.io/rocks-binaries/
        - most of the neovim rocks are not there
        - a lot of treesitter stuff is
        - the fzy rock is there, which was downloaded

--- nvim-env/lua/grs/plugins/infrastructure/lazy_rock.lua
+++ nvim-env/lua/grs/plugins/infrastructure/lazy_rock.lua
@@ -3,17 +3,25 @@
 return {
    { 'folke/lazy.nvim' }, -- Once bootstrapped, lazy.nvim will keep itself updated
 
--- {
---   'vhyrro/luarocks.nvim',
---   priority = 1500, -- Must be first thing loaded
---   opts = {
---     rocks = {
---       'plenary.nvim',
---       'mrcjkb/nvim-lastplace',
---       'nvim-web-devicons', -- configured in plugins.infrastructure.appearance
---     },
---     -- Use LuaRocks I manually installed
---     luarocks_build_args = { '--sysconfig' },
---   },
--- },
+   {
+      -- it appears lazy.nvim is transitioning from LuaRocks to Lux
+      -- rocks are fetched from `https://nvim-neorocks.github.io/rocks-binaries/`
+      -- the fzy plugin is one of the few plugins there
+      'vhyrro/luarocks.nvim',
+      priority = 1500, -- Must be first thing loaded
+      -- here would be where you pin rock versions
+      opts = {
+         rocks = {
+            'fzy'
+         },
+      },
+      -- opts = {
+      --    rocks = {
+      --       'plenary.nvim',
+      --       'nvim-lastplace',
+      --       'nvim-web-devicons',
+      --    },
+      -- },
+      config = true,
+   },
 }
