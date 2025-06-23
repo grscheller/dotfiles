b4e4207779107662664e1872c1b695e1efe201ec
grscheller <geoffrey@scheller.com>
2025-06-23 15:26:01 -0600

    Fixed major from "nvimInstall --clean" rebuild issues
    - not sure why it even worked from the "nvimInstall --install" state

--- home/config/nvim/lua/grs/plugins/infrastructure/telescope.lua
+++ home/config/nvim/lua/grs/plugins/infrastructure/telescope.lua
@@ -1,104 +1,6 @@
 --[[ Telescope - search, filter, find & pick items ]]
 
-local config_telescope = function()
-   local telescope = require 'telescope'
-   local builtin = require 'telescope.builtin'
-   local themes = require 'telescope.themes'
-   local wk = require 'which-key'
-
-   telescope.setup {
-      defaults = {
-         -- mappings while in telescope
-         mappings = {
-            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-         },
-         prompt_prefix = ' ',
-         selection_caret = ' ',
-      },
-      extensions = {
-         file_browser = {
-            theme = 'ivy',
-            hijack_netrw = true,
-         },
-         fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = 'respect_case',
-         },
-         lazy = {
-            theme = 'ivy',
-         },
-         notify = {},
-         ['ui-select'] = {
-            themes.get_dropdown {},
-         },
-      },
-      pickers = {},
-   }
-
-   telescope.load_extension 'file_browser'
-   telescope.load_extension 'fzf'
-   telescope.load_extension 'lazy'
-   telescope.load_extension 'notify'
-   telescope.load_extension 'ui-select'
-
-   wk.add {
-      { '<leader>sh', builtin.help_tags, desc = 'search help' },
-      { '<leader>sk', builtin.keymaps, desc = 'search kymaps' },
-      { '<leader>sf', builtin.find_files, desc = 'search files' },
-      { '<leader>ss', builtin.builtin, desc = 'search select telescope' },
-      { '<leader>sw', builtin.grep_string, desc = 'search current word' },
-      { '<leader>sg', builtin.live_grep, desc = 'search by grep' },
-      { '<leader>sd', builtin.diagnostics, desc = 'search diagnostics' },
-      { '<leader>sr', builtin.resume, desc = 'search resume' },
-      { '<leader>s.', builtin.oldfiles, desc = 'search recent files' },
-      {
-         '<leader>/',
-         function()
-            builtin.current_buffer_fuzzy_find(themes.get_dropdown {
-               winblend = 50,
-               previewer = false,
-            })
-         end,
-         desc = 'fuzzily search in current buffer',
-      },
-      {
-         '<leader>so',
-         function()
-            builtin.live_grep {
-               grep_open_files = true,
-               prompt_title = 'Live Grep in Open Files',
-            }
-         end,
-         desc = 'search open files',
-      },
-   }
-end
-
 return {
-
-   {
-      -- Highly extendable fuzzy finder over lists.
-      --   To open window showing keymaps for current picker,
-      --     Insert mode: <c-/>
-      --     Normal mode: ?
-      'nvim-telescope/telescope.nvim',
-      dependencies = {
-         { 'nvim-lua/plenary.nvim' },
-         { 'nvim-tree/nvim-web-devicons' },
-         { 'nvim-telescope/telescope-ui-select.nvim' },
-         {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make',
-            cond = function()
-               return vim.fn.executable 'make' == 1
-            end,
-         },
-      },
-      config = config_telescope,
-   },
-
    {
       -- File browser extension for telescope. Supports synchronized creation,
       -- deletion, renaming, and moving of files and folders, with LSP integration.
@@ -137,5 +39,100 @@ return {
             desc = 'telescope lazy',
          },
       },
+   },
+
+   {
+      -- Highly extendable fuzzy finder over lists.
+      --   To open window showing keymaps for current picker,
+      --     Insert mode: <c-/>
+      --     Normal mode: ?
+      'nvim-telescope/telescope.nvim',
+      dependencies = {
+         { 'nvim-lua/plenary.nvim' },
+         { 'nvim-tree/nvim-web-devicons' },
+         { 'nvim-telescope/telescope-ui-select.nvim' },
+         {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            cond = function()
+               return vim.fn.executable 'make' == 1
+            end,
+         },
+      },
+      config = function()
+         local telescope = require 'telescope'
+         local builtin = require 'telescope.builtin'
+         local themes = require 'telescope.themes'
+         local wk = require 'which-key'
+
+         telescope.setup {
+            defaults = {
+               -- mappings while in telescope
+               mappings = {
+                  i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+               },
+               prompt_prefix = ' ',
+               selection_caret = ' ',
+            },
+            extensions = {
+               file_browser = {
+                  theme = 'ivy',
+                  hijack_netrw = true,
+               },
+               fzf = {
+                  fuzzy = true,
+                  override_generic_sorter = true,
+                  override_file_sorter = true,
+                  case_mode = 'respect_case',
+               },
+               lazy = {
+                  theme = 'ivy',
+               },
+               notify = {},
+               ['ui-select'] = {
+                  themes.get_dropdown {},
+               },
+            },
+            pickers = {},
+         }
+
+         telescope.load_extension 'file_browser'
+         telescope.load_extension 'fzf'
+         telescope.load_extension 'lazy'
+         telescope.load_extension 'notify'
+         telescope.load_extension 'ui-select'
+
+         wk.add {
+            { '<leader>sh', builtin.help_tags,   desc = 'search help' },
+            { '<leader>sk', builtin.keymaps,     desc = 'search kymaps' },
+            { '<leader>sf', builtin.find_files,  desc = 'search files' },
+            { '<leader>ss', builtin.builtin,     desc = 'search select telescope' },
+            { '<leader>sw', builtin.grep_string, desc = 'search current word' },
+            { '<leader>sg', builtin.live_grep,   desc = 'search by grep' },
+            { '<leader>sd', builtin.diagnostics, desc = 'search diagnostics' },
+            { '<leader>sr', builtin.resume,      desc = 'search resume' },
+            { '<leader>s.', builtin.oldfiles,    desc = 'search recent files' },
+            {
+               '<leader>/',
+               function()
+                  builtin.current_buffer_fuzzy_find(themes.get_dropdown {
+                     winblend = 50,
+                     previewer = false,
+                  })
+               end,
+               desc = 'fuzzily search in current buffer',
+            },
+            {
+               '<leader>so',
+               function()
+                  builtin.live_grep {
+                     grep_open_files = true,
+                     prompt_title = 'Live Grep in Open Files',
+                  }
+               end,
+               desc = 'search open files',
+            },
+         }
+      end,
    },
 }
--- home/config/nvim/lua/grs/plugins/infrastructure/whichkey.lua
+++ home/config/nvim/lua/grs/plugins/infrastructure/whichkey.lua
@@ -1,76 +1,73 @@
 --[[ Needed by multiple plugins or important to infrastructure ]]
 
-local tb = require 'telescope.builtin'
-
-local opts = {
-   plugins = {
-      spelling = {
-         enabled = true,
-         suggestions = 36,
-      },
-   },
-}
-
-local keys = {
-
-   --[[ Which Key related ]]
-
-   {
-      '<leader>?',
-      function()
-         require('which-key').show { global = false }
-      end,
-      desc = 'Buffer Local Keymaps (which-key)',
-   },
-
-   --[[ Treesitter related ]]
-
-   {
-      '<leader>t',
-      '<cmd>TSBufToggle highlight<cr>',
-      desc = 'toggle treesitter highlighting',
-   },
-
-   --[[ Plugin related ]]
-
-   { '<leader>L', '<cmd>Lazy<cr>', desc = 'Lazy gui' },
-   { '<leader>M', '<cmd>Mason<cr>', desc = 'Mason gui' },
-
-   --[[ LSP related ]]
-
-   { 'H', vim.lsp.buf.hover, desc = 'hover document' },
-   { 'K', vim.lsp.buf.signature_help, desc = 'signature help' },
-   { '<leader>F', vim.lsp.buf.format, desc = 'format with LSP' },
-   { '<leader>ca', vim.lsp.buf.code_action, desc = 'code action' },
-   { '<leader>cl', vim.lsp.codelens.refresh, desc = 'code lens refresh' },
-   { '<leader>cr', vim.lsp.codelens.run, desc = 'code lens run' },
-   { 'gd', tb.lsp_definitions, desc = 'definitions' },
-   { 'gD', vim.lsp.buf.declaration, desc = 'goto type decl' },
-   { 'gi', tb.lsp_implementations, desc = 'implementations' },
-   { 'gr', tb.lsp_references, desc = 'references' },
-   { '<leader>r', vim.lsp.buf.rename, desc = 'rename' },
-   { '<leader>ds', tb.lsp_document_symbols, desc = 'document symbols' },
-   { '<leader>ws', tb.lsp_dynamic_workspace_symbols, desc = 'workspace symbols' },
-   { '<leader>wa', vim.lsp.buf.add_workspace_folder, desc = 'add ws folder' },
-   { '<leader>wr', vim.lsp.buf.remove_workspace_folder, desc = 'rm ws folder' },
-   {
-      '<leader>i',
-      function()
-         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-      end,
-      desc = 'toggle inlay hints',
-   },
-
-   --[[ DAP & Diagnostic related ]]
-
-}
-
 return {
    {
       -- Makes keymaps discoverable.
       'folke/which-key.nvim',
       event = 'VeryLazy',
-      opts = opts,
-      keys = keys,
+      opts = {
+         plugins = {
+            spelling = {
+               enabled = true,
+               suggestions = 36,
+            },
+         },
+      },
+      keys = function()
+         local tb = require 'telescope.builtin'
+
+         return {
+
+            --[[ Which Key related ]]
+
+            {
+               '<leader>?',
+               function()
+                  require('which-key').show { global = false }
+               end,
+               desc = 'Buffer Local Keymaps (which-key)',
+            },
+
+            --[[ Treesitter related ]]
+
+            {
+               '<leader>t',
+               '<cmd>TSBufToggle highlight<cr>',
+               desc = 'toggle treesitter highlighting',
+            },
+
+            --[[ Plugin related ]]
+
+            { '<leader>L',  '<cmd>Lazy<cr>',                     desc = 'Lazy gui' },
+            { '<leader>M',  '<cmd>Mason<cr>',                    desc = 'Mason gui' },
+
+            --[[ LSP related ]]
+
+            { 'H',          vim.lsp.buf.hover,                   desc = 'hover document' },
+            { 'K',          vim.lsp.buf.signature_help,          desc = 'signature help' },
+            { '<leader>F',  vim.lsp.buf.format,                  desc = 'format with LSP' },
+            { '<leader>ca', vim.lsp.buf.code_action,             desc = 'code action' },
+            { '<leader>cl', vim.lsp.codelens.refresh,            desc = 'code lens refresh' },
+            { '<leader>cr', vim.lsp.codelens.run,                desc = 'code lens run' },
+            { 'gd',         tb.lsp_definitions,                  desc = 'definitions' },
+            { 'gD',         vim.lsp.buf.declaration,             desc = 'goto type decl' },
+            { 'gi',         tb.lsp_implementations,              desc = 'implementations' },
+            { 'gr',         tb.lsp_references,                   desc = 'references' },
+            { '<leader>r',  vim.lsp.buf.rename,                  desc = 'rename' },
+            { '<leader>ds', tb.lsp_document_symbols,             desc = 'document symbols' },
+            { '<leader>ws', tb.lsp_dynamic_workspace_symbols,    desc = 'workspace symbols' },
+            { '<leader>wa', vim.lsp.buf.add_workspace_folder,    desc = 'add ws folder' },
+            { '<leader>wr', vim.lsp.buf.remove_workspace_folder, desc = 'rm ws folder' },
+            {
+               '<leader>i',
+               function()
+                  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+               end,
+               desc = 'toggle inlay hints',
+            },
+
+            --[[ DAP & Diagnostic related ]]
+         }
+      end,
    },
 }
