ec438c458cf7c5c10e5b1427675c8fdd4901b466
grscheller <geoffrey@scheller.com>
2025-11-28 13:58:49 -0700

    Updated fish abbreviations for git commands.

--- home/config/fish/conf.d/abbrs.fish
+++ home/config/fish/conf.d/abbrs.fish
@@ -2,7 +2,6 @@
 
 # Git related cmds - anything more complicated, I want to think about
 abbr -a ga git add .
-abbr -a gb git branch
 abbr -a gbl 'git branch --list|cat'
 abbr -a gc git commit -S
 abbr -a gco git checkout
@@ -13,10 +12,8 @@ abbr -a gl git log
 abbr -a gm git mv
 abbr -a gp git pull
 abbr -a gs git status
-abbr -a gsu git submodule update
-abbr -a gsm git submodule update --remote --merge
-abbr -a gt 'git tag --list|cat'
-abbr -a gw git switch
+abbr -a gtl 'git tag --list|cat'
+abbr -a gsw git switch
 
 # dotfile installation scripts - single quotes intentional
 abbr -a dfI '$DOTFILES_GIT_REPO/bin/dfInstall'
