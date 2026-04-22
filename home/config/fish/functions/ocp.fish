e4a20fd13c7a877e1e19b21673e40586e18da651
grscheller <geoffrey@scheller.com>
2026-04-22 17:40:52 -0600

    Refining fish/functions/ocp.fish - octopus copy
    - fixed issue with dirpath
    - made copying directly much more explicit

--- home/config/fish/functions/ocp.fish
+++ home/config/fish/functions/ocp.fish
@@ -16,26 +16,41 @@ function ocp --description 'octopus cp - copy one item to many locations'
    set -f cp_flags
    if test -d $item
       set cp_flags '-r'
+      if not string match -qr '/$' -- $item
+         set item $item/
+      end
    end
 
+   set -f dest
    for dest in $dests
       if test -f $dest
          if test -d $item
             printf 'Error: Can not overwrite file "%s" with directory "%s".\n' $dest $item >&2
-            printf 'Abort!\n'
             return 1
          end
       end
-   end
-
-   for dest in $dests
-      if test ! -d (dirname $dest)
+      if not string match -qr '/$' -- $dest
+         if test -d $dest
+            printf 'Error: Trying to copy to a directory without explicitely\n' >&2
+            printf '       indicating a trailing "/".\n' >&2
+            return 1
+         end
+      end
+      if string match -qr '/$' -- $dest
+         if not test -d $dest
+            mkdir -p $dest
+         end
+         echo got to 1
+      else
          mkdir -p (dirname $dest)
+         echo got to 2
       end
    end
 
    for dest in $dests
-      cp $cp_flags $item $dest
+      if not test "$item" -ef  "$dest"
+         cp $cp_flags $item $dest
+      end
    end
 
 end
