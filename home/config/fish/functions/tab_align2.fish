5a0a73c6e8de67d06ece8c87cd9be1e696bd1ec0
grscheller <geoffrey@scheller.com>
2026-06-02 17:10:28 -0600

    Almost got fish function tab_align2.fish
    - debugging needed

--- home/config/fish/functions/tab_align2.fish
+++ home/config/fish/functions/tab_align2.fish
@@ -1,6 +1,5 @@
 function tab_align2 --description 'Align the rows of two columns'
 
-    set -f dollar '$'
     set -f tabstop 8
 
     if argparse s/spaces t/tabstop= -- $argv
@@ -20,8 +19,8 @@ function tab_align2 --description 'Align the rows of two columns'
             end
             return 1
         end
-        eval set -f col1 $dollar$argv[1]
-        eval set -f col2 $dollar$argv[2]
+        eval set -f col1 \$$argv[1]
+        eval set -f col2 \$$argv[2]
     else
         printf 'Error align2: wrong number of arguments given\n' >&2
     end
@@ -34,47 +33,54 @@ function tab_align2 --description 'Align the rows of two columns'
         return 0
     end
 
-    # Determine number of rows to columnate.
-    set -f rows (math max $cnt1,$cnt2)
+    # Determine number of rows to columnate and starting point to add missing rows.
+    set -l num_rows (math max $cnt1,$cnt2)
     set -l diff (math abs \( $cnt1 - $cnt2 \))
-    if test $diff -ne $rows
+    if test $diff -ne $num_rows
         set start (math $diff + 1)
     else
         set start 1
     end
-    # Add empty strings for missing final rows.
-    if test $cnt1 -gt $cnt2
-        set col2[$start..$rows] (
+
+    # Add empty strings for missing final rows for first array.
+    if test $cnt2 -gt $cnt1
+        set col1[$start..$num_rows] (
             string replace 'x' '' (
-                string split '' (string repeat --count $diff 'x')
-            )
-        )
-    else if test $cnt2 -gt $cnt1
-        set col1[$start..$rows] (
-            string replace 'x' ' ' (
-                string split '' (string repeat --count $diff 'x')
+                string split '' (string repeat -n $diff 'x')
             )
         )
     end
 
-    set -f len1
-    set -f len2
-    set -f len
+    # Add empty strings for missing final rows for second array.
+    if test $cnt1 -gt $cnt2
+        set col2[$start..$num_rows] (
+            string replace 'x' '' (
+                string split '' (string repeat -n $diff 'x')
+            )
+        )
+    end
+
+    set -l len
+    set -l stops
+    set -l max_stops 0
+    set -l tabstops
+    set -l addstops
 
     for str in $col1
-        set len (count (string split '' "$str"))
-        set -a len1 len
-        set stops1 (math $len - $len % 1)
+        set -l len (count (string split '' "$str"))
+        set len (math $len - $len % 1)
+        set stops (math $len / $tabstop)
+        set stops (math $stops - $stops % 1 + 1)
+        set -a tabstops $stops
+        set max_stops (math max $stops,$max_stops)
     end
 
-    for str in $col2
-        set len (count (string split '' "$str"))
-        set -a len2 len
-        set stops2 (math $len - $len % 1)
+    for stops in $tabstops
+        set -a addstops (math $max_stops - $stops + 1)
     end
 
-    for ii in (seq $cnt1)
-        printf '%s\t%s\n' $col1[$ii] $col2[$ii]
+    for ii in (seq $num_rows)
+        printf '%s%s%s\n' $col1[$ii] (string repeat -n $addstops[$ii] '	') $col2[$ii]
     end
 
     return 0
