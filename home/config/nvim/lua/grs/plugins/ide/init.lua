4ae10b5ff3784ffc40a97d0bc7f4ca4478293e98
grscheller <geoffrey@scheller.com>
2026-06-19 14:10:45 -0600

    Removed rust plugin configurations.
    - two years out-of-date and a moving target
    - will redo from scratch eventually

--- home/config/nvim/lua/grs/plugins/ide/init.lua
+++ home/config/nvim/lua/grs/plugins/ide/init.lua
@@ -5,5 +5,4 @@ local flatten = require('grs.lib.functional').flattenArray
 return flatten {
    require 'grs.plugins.ide.dap',
    require 'grs.plugins.ide.ide',
-   require 'grs.plugins.ide.rust',
 }
