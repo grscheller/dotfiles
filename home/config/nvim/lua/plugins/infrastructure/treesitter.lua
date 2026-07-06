fce6143c719bce14fd0f3db00c8b041650ff6520
grscheller <geoffrey@scheller.com>
2026-07-06 16:07:09 -0600

    Moved Treesitter configuration to config.treesitter
    - new file lua/config/treesitter.lua

--- home/config/nvim/lua/plugins/infrastructure/treesitter.lua
+++ home/config/nvim/lua/plugins/infrastructure/treesitter.lua
@@ -1,53 +1,8 @@
 --[[ Config neovim built-in treesitter & install language modules to it ]]
 
-local ensure_installed = {
-   'awk',
-   'bash',
-   'c',
-   'clojure',
-   'cmake',
-   'cpp',
-   'css',
-   'diff',
-   'fish',
-   'fortran',
-   'git_rebase',
-   'gitattributes',
-   'gitcommit',
-   'gitignore',
-   'go',
-   'haskell',
-   'html',
-   'java',
-   'javascript',
-   'json',
-   'julia',
-   'kotlin',
-   'llvm',
-   'lua',
-   'make',
-   'markdown',
-   'markdown_inline',
-   'ocaml',
-   'python',
-   'query',
-   'r',
-   'racket',
-   'regex',
-   'rust',
-   'scala',
-   'toml',
-   'tsx',
-   'typescript',
-   'vim',
-   'vimdoc',
-   'yaml',
-   'zig',
-}
-
 local config_treesitter = function()
    -- Install parsers (asynchronous, idempotent)
-   require('nvim-treesitter').install(ensure_installed)
+   require('nvim-treesitter').install(require('config.treesitter').ensure_installed)
 
    -- Enable treesitter features per-buffer
    vim.api.nvim_create_autocmd('FileType', {
