65a469e5e209d66cbb858dee50878526119e814c
grscheller <geoffrey@scheller.com>
2026-07-12 15:34:28 -0600

    Now blink.cmp and blink.pairs on latest V2 versions
    - Now on latest V2 versions
      - V1.* didn't play nice with Noice
      - Saw that Noice maintainer folke was a Blink contributor

--- home/config/nvim/lua/plugins/visual_editing.lua
+++ home/config/nvim/lua/plugins/visual_editing.lua
@@ -1,117 +1,19 @@
---[[ Visual editing:
+--[[ Visual editing: plugins
 
-     - completion engine
+     - Completion engine
      - Matching pairs
      - Indentation indication
      - Navigation
      - Colorize names.           ]]
 
-local blink_opts = {
-   -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-   -- 'super-tab' for mappings similar to vscode (tab to accept)
-   -- 'enter' for enter to accept
-   -- 'none' for no mappings
-   --
-   -- All presets have the following mappings:
-   -- C-space: Open menu or open docs if already open
-   -- C-n/C-p or Up/Down: Select next/previous item
-   -- C-e: Hide menu
-   -- C-k: Toggle signature help (if signature.enabled = true)
-   --
-   -- See :h blink-cmp-config-keymap for defining your own keymap
-   keymap = { preset = 'enter' },
-   -- Adjusts spacing to ensure icons are aligned - 'mono' or 'normal'
-   appearance = { nerd_font_variant = 'mono' },
-   completion = {
-      documentation = { auto_show = false },
-      menu = {
-         draw = {
-            components = {
-               kind_icon = {
-                  text = function(ctx)
-                     local icon = ctx.kind_icon
-                     if
-                         vim.tbl_contains({ 'Path' }, ctx.source_name)
-                     then
-                        local dev_icon, _ =
-                            require('nvim-web-devicons').get_icon(
-                               ctx.label
-                            )
-                        if dev_icon then
-                           icon = dev_icon
-                        end
-                     else
-                        icon = require('lspkind').symbolic(ctx.kind, {
-                           mode = 'symbol',
-                        })
-                     end
-                     return icon .. ctx.icon_gap
-                  end,
-                  highlight = function(ctx)
-                     local hl = ctx.kind_hl
-                     if
-                         vim.tbl_contains({ 'Path' }, ctx.source_name)
-                     then
-                        local dev_icon, dev_hl = require(
-                           'nvim-web-devicons'
-                        ).get_icon(ctx.label)
-                        if dev_icon then
-                           hl = dev_hl
-                        end
-                     end
-                     return hl
-                  end,
-               },
-            },
-         },
-      },
-   },
-   sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
-      providers = {
-         buffer = {
-            opts = {
-               get_bufnrs = function()
-                  return vim.tbl_filter(function(bufnr)
-                     return vim.bo[bufnr].buftype == ''
-                  end, vim.api.nvim_list_bufs())
-               end,
-            },
-         },
-         ripgrep = {
-            module = 'blink-cmp-rg',
-            name = 'Ripgrep',
-            opts = {
-               prefix_min_len = 3,
-               get_command = function(_, prefix)
-                  return {
-                     'rg',
-                     '--no-config',
-                     '--json',
-                     '--word-regexp',
-                     '--',
-                     prefix .. '[\\w_-]+',
-                     vim.fs.root(0, '.git') or vim.fn.getcwd(),
-                  }
-               end,
-               get_prefix = function(context)
-                  return context.line
-                      :sub(1, context.cursor[2])
-                      :match '[%w_-]+$' or ''
-               end,
-            },
-         },
-      },
-   },
-   fuzzy = { implementation = 'prefer_rust_with_warning' },
-}
-
 return {
    -- Completion engine.
    {
       [1] = 'saghen/blink.cmp',
-      version = '1.*',
-      event = { 'InsertEnter', 'CmdlineEnter' },
+      event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
+      build = function()
+         require('blink.cmp').build():pwait()
+      end,
       dependencies = {
          'saghen/blink.lib',
          'niuiic/blink-cmp-rg.nvim',
@@ -119,22 +21,131 @@ return {
          'nvim-tree/nvim-web-devicons',
          'rafamadriz/friendly-snippets',
       },
-      opts = blink_opts,
+      version = '*',
+      opts = {
+         -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+         -- 'super-tab' for mappings similar to vscode (tab to accept)
+         -- 'enter' for enter to accept
+         -- 'none' for no mappings
+         --
+         -- All presets have the following mappings:
+         -- C-space: Open menu or open docs if already open
+         -- C-n/C-p or Up/Down: Select next/previous item
+         -- C-e: Hide menu
+         -- C-k: Toggle signature help (if signature.enabled = true)
+         --
+         -- See :h blink-cmp-config-keymap for defining your own keymap
+         keymap = { preset = 'enter' },
+         appearance = { nerd_font_variant = 'mono' },
+         completion = {
+            documentation = { auto_show = false },
+            menu = {
+               draw = {
+                  components = {
+                     kind_icon = {
+                        text = function(ctx)
+                           local icon = ctx.kind_icon
+                           if
+                              vim.tbl_contains(
+                                 { 'Path' },
+                                 ctx.source_name
+                              )
+                           then
+                              local dev_icon, _ =
+                                 require('nvim-web-devicons').get_icon(
+                                    ctx.label
+                                 )
+                              if dev_icon then
+                                 icon = dev_icon
+                              end
+                           else
+                              icon = require('lspkind').symbol_map[ctx.kind]
+                                 or ''
+                           end
+                           return icon .. ctx.icon_gap
+                        end,
+                        highlight = function(ctx)
+                           local hl = ctx.kind_hl
+                           if
+                              vim.tbl_contains(
+                                 { 'Path' },
+                                 ctx.source_name
+                              )
+                           then
+                              local dev_icon, dev_hl =
+                                 require('nvim-web-devicons').get_icon(
+                                    ctx.label
+                                 )
+                              if dev_icon then
+                                 hl = dev_hl
+                              end
+                           end
+                           return hl
+                        end,
+                     },
+                  },
+               },
+            },
+         },
+         sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
+            providers = {
+               buffer = {
+                  opts = {
+                     get_bufnrs = function()
+                        return vim.tbl_filter(function(bufnr)
+                           return vim.bo[bufnr].buftype == ''
+                        end, vim.api.nvim_list_bufs())
+                     end,
+                  },
+               },
+               ripgrep = {
+                  module = 'blink-cmp-rg',
+                  name = 'Ripgrep',
+                  opts = {
+                     prefix_min_len = 3,
+                     get_command = function(_, prefix)
+                        return {
+                           'rg',
+                           '--no-config',
+                           '--json',
+                           '--word-regexp',
+                           '--',
+                           prefix .. '[\\w_-]+',
+                           vim.fs.root(0, '.git') or vim.fn.getcwd(),
+                        }
+                     end,
+                     get_prefix = function(context)
+                        return context.line
+                           :sub(1, context.cursor[2])
+                           :match '[%w_-]+$' or ''
+                     end,
+                  },
+               },
+            },
+         },
+         fuzzy = { implementation = 'prefer_rust_with_warning' },
+      },
    },
 
    -- Matching pairs manipulation.
    {
       'saghen/blink.pairs',
-      dependencies = 'saghen/blink.lib',
-      version = '*', -- download pre-built binaries from GitHub releases
-      build = function() require('blink.pairs').download():pwait(60000) end,
+      dependencies = {
+         'saghen/blink.lib',
+         'rafamadriz/friendly-snippets',
+      },
+      version = '*',
+      event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
+      build = function()
+         require('blink.pairs').build():pwait()
+      end,
       opts = {
          mappings = {
             enabled = true,
-            cmdline = false, -- for now let noice fully own the cmdline
+            cmdline = true, -- might not play nice with noice
          },
       },
-
    },
 
    -- Show line indentations when editing.
