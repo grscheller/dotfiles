fcf7d24707ff2f7da5ac9595bb69ff6382368de3
grscheller <geoffrey@scheller.com>
2026-07-12 19:02:06 -0600

    Dropped blink-cmp back to version 1.*
    - made lazy while trying to get version 2.0.0 prerelease to work
    - problem with Noice went away

--- home/config/nvim/lua/plugins/visual_editing.lua
+++ home/config/nvim/lua/plugins/visual_editing.lua
@@ -10,31 +10,16 @@ return {
    -- Completion engine.
    {
       [1] = 'saghen/blink.cmp',
-      event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
-      build = function()
-         require('blink.cmp').build():pwait()
-      end,
       dependencies = {
-         'saghen/blink.lib',
+         'saghen/blink.lib',  -- will be needed for v2.0.0
          'niuiic/blink-cmp-rg.nvim',
          'onsails/lspkind.nvim',
          'nvim-tree/nvim-web-devicons',
          'rafamadriz/friendly-snippets',
       },
-      version = '*',
+      version = '1.*',
+      event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
       opts = {
-         -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-         -- 'super-tab' for mappings similar to vscode (tab to accept)
-         -- 'enter' for enter to accept
-         -- 'none' for no mappings
-         --
-         -- All presets have the following mappings:
-         -- C-space: Open menu or open docs if already open
-         -- C-n/C-p or Up/Down: Select next/previous item
-         -- C-e: Hide menu
-         -- C-k: Toggle signature help (if signature.enabled = true)
-         --
-         -- See :h blink-cmp-config-keymap for defining your own keymap
          keymap = { preset = 'enter' },
          appearance = { nerd_font_variant = 'mono' },
          completion = {
@@ -124,7 +109,6 @@ return {
                },
             },
          },
-         fuzzy = { implementation = 'prefer_rust_with_warning' },
       },
    },
 
@@ -143,7 +127,7 @@ return {
       opts = {
          mappings = {
             enabled = true,
-            cmdline = true, -- might not play nice with noice
+            cmdline = true,
          },
       },
    },
